45a54955ed4d8932633f3e4fc86a78b8de109b74
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-11-02 22:41:14 -0300

    Update main.py

--- PythonBrasil/main.py
+++ PythonBrasil/main.py
@@ -1,10 +1,37 @@
-
 from Acesso_cep import BuscaEndereco
+from TelefonesBr import Telefone
+from cpf_cnpj import Documento
+from DatasBr import Datas
 
-cep = 72120070
-objeto_cep = BuscaEndereco(cep)
-bairro, cidade, uf = objeto_cep.acessa_via_cep()
+opcao = int(input(''' 
+1 - Cep
+2 - Telefone
+3 - CPf / CNPJ
+4 - Salvar data atual
+Digite o número da opção desajada: '''))
 
-print(bairro, cidade, uf)
+match opcao:
+    case 1:
+        cep = int(input("\nDigite o CEP: "))
+        objeto_cep = BuscaEndereco(cep)
+        uf, localidade, cep = objeto_cep.acessa_via_cep()
+        print(f"\n{uf}, {localidade}, {cep}")
 
-print(objeto_cep)
+    case 2:
+        telefone = input("\nDigite o Telefone: ")
+        objeto_telefone = Telefone(telefone)
+        print(f"\n{objeto_telefone.formata()}")
+
+    case 3:
+        documento = input("\nDigite o CPF/CNPJ: ")
+        doc = Documento()
+        print(f"\n{doc.cria_documento(documento)}")
+    
+    case 4:
+        data = Datas()
+        print(f'''\nAno: {data.ano_cadastro()},
+Mês: {data.mes_cadastro()},
+Dia: {data.dia_cadastro()},
+Dia da semana: {data.dia_semana()},
+Data: {data.formata_data()},
+Tempo: {data.tempo_cadastro()}''')
